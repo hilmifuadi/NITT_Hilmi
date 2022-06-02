bb94971e38516066e70af13b5c667252e2305c75
hilmifuadi <hilmifuadi@gmail.com>
2022-06-02 13:53:41 +0700

    dbservice

--- Services/DbService.Service.cs
+++ Services/DbService.Service.cs
@@ -1,17 +1,52 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
+using System;  
+using System.Collections.Generic;  
+using System.Data;  
+using System.Data.SqlClient;  
+using System.Linq;  
+using System.Web; 
 
 namespace myapp.Service
 {
     public class DbService
     {
-        private string ConnectionString {get;set;}
-        public string ExecuteSQL(string query)
-        {
-            SqlConnection conn = new SqlConnection();
+        string ConnectionString = "";  
+        SqlConnection con;  
+  
+        public void OpenConection()  
+        {  
+            con = new SqlConnection(ConnectionString);  
+            con.Open();  
+        }  
+  
+  
+        public void CloseConnection()  
+        {  
+            con.Close();  
+        }  
+  
+  
+        public void ExecuteQueries(string Query_)  
+        {  
+            SqlCommand cmd = new SqlCommand(Query_,con);  
+            cmd.ExecuteNonQuery();  
+        }  
+  
+  
+        public SqlDataReader DataReader(string Query_)  
+        {  
+            SqlCommand cmd = new SqlCommand(Query_,con);  
+            SqlDataReader dr = cmd.ExecuteReader();  
+            return dr;  
+        }  
+  
+  
+        public object ShowDataInGridView(string Query_)  
+        {  
+            SqlDataAdapter dr = new SqlDataAdapter(Query_, ConnectionString);  
+            DataSet ds = new DataSet();  
+            dr.Fill(ds);  
+            object dataum = ds.Tables[0];  
+            return dataum;  
         }
         
     }
